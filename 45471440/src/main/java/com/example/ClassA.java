f1804c1dd6314aff2b6b88d836bff68822bdd4ee
Szymon Stepniak <szymon.stepniak@gmail.com>
2017-08-03 00:02:55 +0200

    45471440 answer

---
+++ 45471440/src/main/java/com/example/ClassA.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+final class ClassA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!" + NumberUtils.toInt("234"));
+    }
+}
---
+++ 45471440/src/main/java/com/example/ClassB.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+final class ClassB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!" + NumberUtils.toInt("2343321"));
+    }
+}
