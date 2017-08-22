8f56732f0d14a3566ecc8dab471e8d1399d70034
Szymon Stepniak <szymon.stepniak@gmail.com>
2017-08-22 06:37:40 +0200

    45650417 answer updated

--- 45650417/src/avg.groovy
+++ 45650417/src/avg.groovy
@@ -1,10 +1,11 @@
+import java.nio.file.Files
+
 File myFile = new File("/tmp/numbers.txt")
 
-def avg1 = myFile.readLines()
-        .stream()
-        .mapToInt({ s -> s.toInteger() })
-        .average()
-        .getAsDouble()
+    def avg1 =  Files.lines(myFile.toPath())
+            .mapToInt({ s -> s.toInteger() })
+            .average()
+            .getAsDouble()
 
 def lines = myFile.readLines()
 def avg2 = lines.sum({ it.toInteger() }) / lines.size()
